0c77ceb7dbd527a2257956d9d8c787ff15eed57b
Aashish Pandey <aashishkpandey@gmail.com>
2016-08-24 18:59:37 +0530

    correction

--- ui.R
+++ ui.R
@@ -29,12 +29,12 @@ shinyUI(pageWithSidebar(
                 tabPanel("Overview",
                          h4(p("How to use this shiny application")),
                          p("This shiny application require one data input from the user. To do so, click on the Browse (in left side-bar panel) and upload the csv data input file.
-                           Note that this application can read only csv file(comma delimited file), so if you don't have csv input data file, first convert your data in csv format 
+                           Note that this application can read only csv file (comma delimited file), so if you don't have csv input data file, first convert your data in csv format 
                            and then proceed. Make sure you have top row as variable names.",align="justify"),
                          p("Once csv file is uploaded successfully, variables in the data file will reflect in left-side Data Selection Panel. Now you can select 
-                            dependent variable (Y Variable) from drop-down menu. By default all other remaining variables will be selected as explanotry variables (X variables). 
-                              If you want to drop any variable from explanotry variables, just uncheck that variable and it will be dropped from the model. 
-                            If any of the variables selected in explanotry variables is a factor variable, you can define that variable as factor variable just
+                            dependent variable (Y Variable) from drop-down menu. By default all other remaining variables will be selected as explanatory variables (X variables). 
+                              If you want to drop any variable from explanatory variables, just uncheck that variable and it will be dropped from the model. 
+                            If any of the variables selected in explanatory variables is a factor variable, you can define that variable as factor variable just
                             by selecting that variable in the last list of variables
                            ",align="justify"),
                          br(),
