9fabd4595cb21a46fd431d41f5eba208521bbe9c
Sudhir Voleti <sudhir_voleti@isb.edu>
2019-01-29 16:17:57 +0530

    Update dependency-ols-shinyapp.R

--- dependency-ols-shinyapp.R
+++ dependency-ols-shinyapp.R
@@ -1,10 +1,10 @@
 
-try(require("shiny")||install.packages("shiny"))
-try(require("pastecs")||install.packages("pastecs"))
-try(require("RColorBrewer")||install.packages("RColorBrewer"))
-try(require("Hmisc")||install.packages("Hmisc"))
-try(require("ggplot2")||install.packages("ggplot2"))
-try(require("reshape2")||install.packages("reshape2"))
+if(!require("shiny")) {install.packages("shiny")}
+if(!require("pastecs")){install.packages("pastecs")}
+if(!require("RColorBrewer")){install.packages("RColorBrewer")}
+if(!require("Hmisc")){install.packages("Hmisc")}
+if(!require("ggplot2")){install.packages("ggplot2")}
+if(!require("reshape2")){install.packages("reshape2")}
 
 library(shiny)
 library(pastecs)
