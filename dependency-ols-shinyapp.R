8f65ee42c73bce8bd547b126366ad7439e0613e5
Shivani Srivastava <shivani141296@gmail.com>
2022-07-19 13:58:44 +0530

    Update dependency-ols-shinyapp.R
    Added updated library installation + loading commands

--- dependency-ols-shinyapp.R
+++ dependency-ols-shinyapp.R
@@ -5,6 +5,11 @@ if(!require("RColorBrewer")){install.packages("RColorBrewer")}
 if(!require("Hmisc")){install.packages("Hmisc")}
 if(!require("ggplot2")){install.packages("ggplot2")}
 if(!require("reshape2")){install.packages("reshape2")}
+if(!require("stats")){install.packages("stats")}
+if(!require("skedastic")){install.packages("skedastic")}
+if(!require("lmtest")){install.packages("lmtest")}
+if(!require("olsrr")){install.packages("olsrr")}
+
 
 library(shiny)
 library(pastecs)
@@ -12,3 +17,7 @@ library(RColorBrewer)
 library(Hmisc)
 library(ggplot2)
 library(reshape2)
+library(stats)
+library(skedastic)
+library(lmtest)
+library(olsrr)
